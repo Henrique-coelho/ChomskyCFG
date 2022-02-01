b469dc8fbd4f0d0953b1f082846a80e3975a5620
Rodrigo Lopes <rodrigolopesferreira4@gmail.com>
2022-02-01 17:48:21 -0300

    Adicionado Permutação

--- src/main/java/operations/impl/OperationsImpl.java
+++ src/main/java/operations/impl/OperationsImpl.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import operations.Operations;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,21 +15,98 @@ public class OperationsImpl implements Operations {
 	
     @Override
     public CFGrammar removeUselessVar(CFGrammar cfGrammar) {
-        var lambdaList = cfGrammar.getRules().stream()
-                .map(l -> {
-                    List<String> returnList = new ArrayList<>();
-                    l.stream().forEach(rule -> {
-                        if(rule.contains("#")) {
-                            returnList.add(l.get(0));
-                        }
-                    });
-                    return returnList;
-                })
-                .flatMap(t -> t.stream())
-                .collect(Collectors.toList());
+    	List<String> lambdListOG;
+    	List<String> lambdaList = new ArrayList<>();
+        do {
+        	lambdListOG = lambdaList;
+        	// Identifica novas regras nulas
+	    	lambdaList = cfGrammar.getRules().stream()
+	                .map(l -> {
+	                    List<String> returnList = new ArrayList<>();
+	                    l.stream().forEach(rule -> {
+	                        if(rule.contains("#")) {
+	                            returnList.add(l.get(0));
+	                        }
+	                    });
+	                    return returnList;
+	                })
+	                .flatMap(t -> t.stream())
+	                .collect(Collectors.toList());
+	    	
+	    	// Atualiza Gramatica
+	    	List<List<String>> newRules = new ArrayList<>(); 
+	    	for(String c : lambdaList){
+	    		for(List<String> l : cfGrammar.getRules()) {
+	    			if(!newRules.contains(l))
+	    				newRules.add(l);
+	    			if(l.get(1).contains(c)) {
+	    				
+	    			}
+	    		}
+	    	};
+	    	
+	    	List<Integer> t = new ArrayList<>();
+	    	t.add(1);
+	    	t.add(2);
+	    	t.add(3);
+	    	
+	    	List<HashSet<Integer>> p = permute(t);
+	    	
+	    	List<String> llfinal = lambdaList;
+	    	CFGrammar cfAux = cfGrammar;
+	    	cfGrammar.getRules().stream()
+	    		.map(l -> {
+	    			l.get(1);
+	    			Boolean aux = false;
+	    			
+	    			return null;
+	    		});
+	    	
+        } while (!new HashSet<>(lambdaList).equals(new HashSet<>(lambdListOG)));
         return cfGrammar;
     }
 
+    
+    
+    
+    
+    private List<HashSet<Integer>> permute(List<Integer> mainList) {
+    	var p = permute(new ArrayList<>(), mainList);
+    	var q = p.stream()
+    			.map(l -> {
+    				return new HashSet<>(l);
+    			})
+    			.distinct()
+    			.collect(Collectors.toList());
+    	q.add(new HashSet<>());
+    	return q;
+    }
+    
+    private List<List<Integer>> permute(List<Integer> midList, List<Integer> mainList) {
+    	int size = mainList.size();
+    	List<List<Integer>> combinations = new ArrayList<>();
+    	List<Integer> auxList;
+    	
+    	for(int i=0;i<size;i++) {
+    		auxList = new ArrayList<>(midList);
+    		auxList.add(mainList.get(i));
+    		if(auxList.size() != size) {
+    			for(List<Integer> comb : permute(auxList,mainList)) {
+    				combinations.add(comb);
+    			}
+    		}
+    		else {
+    			combinations.add(auxList);
+    		}
+    		
+    	}
+    	return combinations;
+    }
+    
+    
+    
+    
+    
     @Override
     public CFGrammar removeUnitaryRules(CFGrammar cfGrammar) {
         //TODO
