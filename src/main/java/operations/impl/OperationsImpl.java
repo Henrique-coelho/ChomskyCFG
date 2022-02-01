4aaa2c3c872a3989986f60c5bd47a1bbe4a0e98a
Henrique-coelho <h8mendes@gmail.com>
2022-02-01 14:40:33 -0300

    get lambdaList

--- src/main/java/operations/impl/OperationsImpl.java
+++ src/main/java/operations/impl/OperationsImpl.java
@@ -4,12 +4,27 @@ import dto.CFGrammar;
 import lombok.NoArgsConstructor;
 import operations.Operations;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @NoArgsConstructor
 public class OperationsImpl implements Operations {
 	
     @Override
     public CFGrammar removeUselessVar(CFGrammar cfGrammar) {
-        //TODO
+        var lambdaList = cfGrammar.getRules().stream()
+                .map(l -> {
+                    List<String> returnList = new ArrayList<>();
+                    l.stream().forEach(rule -> {
+                        if(rule.contains("#")) {
+                            returnList.add(l.get(0));
+                        }
+                    });
+                    return returnList;
+                })
+                .flatMap(t -> t.stream())
+                .collect(Collectors.toList());
         return cfGrammar;
     }
 
